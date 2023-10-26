e2ed7dc0153946e89e5a9249144812f73cc7c955
Abdullah <60250106+abdullah-785@users.noreply.github.com>
2023-10-26 19:23:42 +0500

    Review and Menu Screen

--- flutter_practise/lib/Screens/restaurant_detail.dart
+++ flutter_practise/lib/Screens/restaurant_detail.dart
@@ -288,8 +288,104 @@ class _RestaurantDetailState extends State<RestaurantDetail> {
                 )
               ]),
             ),
-            const Column(
-              children: [Text("Second Tab View")],
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    // width: MediaQuery.of(context).size.width * 1,
+                    height: 125,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [
+                          BoxShadow(
+                              offset: Offset(5, 5),
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2,
+                              blurRadius: 20)
+                        ]),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 100,
+                          height: 130,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image(
+                                fit: BoxFit.cover,
+                                image: NetworkImage(
+                                    "https://firebasestorage.googleapis.com/v0/b/flutterpractise-6898a.appspot.com/o/great-for-sharing.jpg?alt=media&token=293f7b33-ee27-4f90-a7fc-e04e33d785c5&_gl=1*1330j4f*_ga*MTIxMjcyNzYwNy4xNjk4MTI2MzMz*_ga_CW55HF8NVT*MTY5ODI5ODkyMS4xMS4xLjE2OTgyOTg5OTEuNTEuMC4w")),
+                          ),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * .68,
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              // mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Classic Margarita",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Expanded(
+                                  child: Text(
+                                      "Tequila, lime juice, and triple sec, served on the rocks with a salt rim."),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    RichText(
+                                      text: const TextSpan(
+                                        text: '\$',
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.w500),
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                              text: '10',
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.w500)),
+                                        ],
+                                      ),
+                                    ),
+                                    Spacer(),
+                                    Container(
+                                      alignment: Alignment.center,
+                                      width: 90,
+                                      height: 28,
+                                      decoration: BoxDecoration(
+                                          color: InternalYellow,
+                                          borderRadius:
+                                              BorderRadius.circular(20)),
+                                      child: Text(
+                                        "Add to cart",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 12),
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 12.0),
@@ -392,29 +488,17 @@ class _RestaurantDetailState extends State<RestaurantDetail> {
                           )
                         ],
                       ),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Container(
-                                alignment: Alignment.center,
-                                width: 110,
-                                height: 35,
-                                decoration: BoxDecoration(
-                                    color:
-                                        const Color.fromARGB(255, 240, 163, 35),
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: const Text(
-                                  "Post Review",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              const Column(
-                                children: [Text("Williams Ebenezer")],
-                              )
-                            ],
-                          )
-                        ],
+                      const SizedBox(
+                        height: 50,
+                      ),
+                      const SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Column(
+                          children: [
+                            CustomerReviewsWidget(),
+                            CustomerReviewsWidget(),
+                          ],
+                        ),
                       )
                     ],
                   )
@@ -422,6 +506,76 @@ class _RestaurantDetailState extends State<RestaurantDetail> {
               ),
             )
           ])),
+    );
+  }
+}
+
+class CustomerReviewsWidget extends StatelessWidget {
+  const CustomerReviewsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 30.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 35,
+                    height: 35,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg")),
+                      shape: BoxShape.circle,
+                      color: Colors.blue,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Column(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text("Williams Ebenezer",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500, fontSize: 14)),
+                      RatingBarIndicator(
+                          rating: 4.4,
+                          itemCount: 5,
+                          itemSize: 10.0,
+                          itemBuilder: (context, _) => const Icon(
+                                Icons.star,
+                                color: Color.fromARGB(255, 240, 163, 35),
+                              )),
+                    ],
+                  ),
+                ],
+              ),
+              Text(
+                "Sep 13, 2023",
+                style: TextStyle(fontSize: 12, color: Colors.grey[600]),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          const Text(
+            textAlign: TextAlign.justify,
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+            style: TextStyle(fontSize: 11),
+          )
+        ],
+      ),
     );
   }
 }
--- flutter_practise/lib/main.dart
+++ flutter_practise/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       debugShowCheckedModeBanner: false,
       home: Login(),
     );
